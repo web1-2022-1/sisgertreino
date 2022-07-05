117874bd76835f313aabe073e55b82af889be18f
Murilo763 <vianamurilo736@gmail.com>
2022-07-05 11:46:31 -0300

    adicionando arquivo config.php no gitignore

---
+++ BD/config.php
@@ -0,0 +1,11 @@
+<?php
+##salvar como config.php
+
+##arquivo que contem variaveis que serão usadas no arquivo de configuração do BD
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('BASE', 'porigym');
+
+?>
