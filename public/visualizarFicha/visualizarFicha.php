7be18eae5b26f47a239c2d373df8b1a36d3f177e
Joao Pedro <pachecogbi@outlook.com>
2022-07-06 19:33:16 -0300

    Alterando Titulo da lista de fichas

--- public/visualizarFicha/visualizarFicha.php
+++ public/visualizarFicha/visualizarFicha.php
@@ -27,7 +27,7 @@
 
         </div>
         <div class="conteudo" id="scrollbar">
-            <h1 class="edit-title">Lista de treinos</h1>
+            <h1 class="edit-title">Lista de Fichas</h1>
             <table class="table" border="1">
                 <thead>
                     <th class="table_head">Nome</th>
