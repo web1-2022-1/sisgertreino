322253396c71db1074e910fdfe2bc2f7eeae71dd
Murilo763 <vianamurilo736@gmail.com>
2022-07-07 01:04:45 -0300

    overflow adicionado na tag main de visualizar treino

--- public/visualizarTreino/treinoAluno.php
+++ public/visualizarTreino/treinoAluno.php
@@ -1,6 +1,6 @@
-<?php
+<!--<?php
 require_once '../../Controller/Treino/CrudTreino.php';
-?>
+?>-->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,7 +15,7 @@ require_once '../../Controller/Treino/CrudTreino.php';
     <title>PoriGYM</title>
 
 </head>
-<?php
+<!--<?php
 if($_POST['cpf_aluno']==NULL){
     $post_cpf_aluno = explode(':', $_POST['nome_aluno']);
     $cpf_aluno = intval(substr($post_cpf_aluno[1], 1, -2));
@@ -25,11 +25,11 @@ if($_POST['cpf_aluno']==NULL){
     $treino = new CrudTreino;
     $treino->setCpf_aluno($_POST['cpf_aluno']);
 }
-?>
+?>-->
 
 <body>
 
-    <main>
+    <main id="scrollbar">
 
         <div class="sequencia_topo">
 
@@ -41,7 +41,7 @@ if($_POST['cpf_aluno']==NULL){
            
 
         </div>
-        <div class="conteudo" id="scrollbar">
+        <div class="conteudo">
             <h1 class="edit-title">Lista de treinos</h1>
                 <div class="button_find">
                     <button type="submit">Criar Novo Treino</button>
@@ -53,7 +53,57 @@ if($_POST['cpf_aluno']==NULL){
                     <th class="table_head">Ações</th>
                 </thead>
                 <tbody>
-                    <?php
+                    <p> 
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit aperiam, dolorem magni molestiae enim nisi laudantium deserunt asperiores soluta excepturi similique, nulla quaerat doloremque eius reiciendis voluptatibus animi, obcaecati corrupti.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sequi dignissimos quo repellat exercitationem accusamus dolorum ab, ullam voluptates dicta eum neque assumenda? Inventore soluta explicabo ea asperiores voluptates totam.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum aut magni laboriosam ipsam hic repellat aperiam delectus ipsum eveniet possimus sapiente quae, adipisci tempora voluptas provident distinctio! Iusto, consectetur consequatur!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ad corporis repellendus ducimus quas molestiae tenetur quos necessitatibus commodi reiciendis libero possimus, reprehenderit adipisci dignissimos illo non aliquid blanditiis suscipit!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis numquam ad rerum corrupti nisi tempore ratione quisquam velit laudantium sit voluptates expedita, labore quasi, dolorum accusamus necessitatibus sequi. Magni, architecto.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum sit voluptatem facilis excepturi officiis dolores. Perspiciatis amet ipsum dolores ipsam, repellat atque. Soluta qui nostrum quae culpa quod voluptate ad!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim perferendis, nihil adipisci ratione exercitationem earum aliquid minima optio ipsa commodi culpa ea delectus pariatur sequi id corrupti, ad dolores dicta?
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus reiciendis veniam quas, labore harum voluptas perferendis autem consequuntur nesciunt consectetur voluptatem explicabo ad laudantium, est inventore minima, illum obcaecati dignissimos!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium molestiae alias repellat deleniti. Possimus, et. Esse, eveniet nemo porro, quis ullam magnam atque maxime iusto ratione maiores corporis, facilis ut!
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, maiores, distinctio similique tempora quod repellat voluptas, eligendi dolore non eum pariatur exercitationem perspiciatis delectus eius laboriosam dolores! Placeat, necessitatibus obcaecati.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut molestiae harum animi perspiciatis tenetur quam, dolorem, quis aperiam repudiandae assumenda modi autem quidem consequuntur hic alias at? Libero, ipsum aperiam?
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem culpa earum fuga veniam non eos in, sed nisi illo cumque aspernatur assumenda voluptates, sunt soluta, id magni placeat explicabo dignissimos!
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit aperiam, dolorem magni molestiae enim nisi laudantium deserunt asperiores soluta excepturi similique, nulla quaerat doloremque eius reiciendis voluptatibus animi, obcaecati corrupti.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sequi dignissimos quo repellat exercitationem accusamus dolorum ab, ullam voluptates dicta eum neque assumenda? Inventore soluta explicabo ea asperiores voluptates totam.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum aut magni laboriosam ipsam hic repellat aperiam delectus ipsum eveniet possimus sapiente quae, adipisci tempora voluptas provident distinctio! Iusto, consectetur consequatur!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ad corporis repellendus ducimus quas molestiae tenetur quos necessitatibus commodi reiciendis libero possimus, reprehenderit adipisci dignissimos illo non aliquid blanditiis suscipit!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis numquam ad rerum corrupti nisi tempore ratione quisquam velit laudantium sit voluptates expedita, labore quasi, dolorum accusamus necessitatibus sequi. Magni, architecto.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum sit voluptatem facilis excepturi officiis dolores. Perspiciatis amet ipsum dolores ipsam, repellat atque. Soluta qui nostrum quae culpa quod voluptate ad!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim perferendis, nihil adipisci ratione exercitationem earum aliquid minima optio ipsa commodi culpa ea delectus pariatur sequi id corrupti, ad dolores dicta?
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus reiciendis veniam quas, labore harum voluptas perferendis autem consequuntur nesciunt consectetur voluptatem explicabo ad laudantium, est inventore minima, illum obcaecati dignissimos!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium molestiae alias repellat deleniti. Possimus, et. Esse, eveniet nemo porro, quis ullam magnam atque maxime iusto ratione maiores corporis, facilis ut!
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, maiores, distinctio similique tempora quod repellat voluptas, eligendi dolore non eum pariatur exercitationem perspiciatis delectus eius laboriosam dolores! Placeat, necessitatibus obcaecati.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut molestiae harum animi perspiciatis tenetur quam, dolorem, quis aperiam repudiandae assumenda modi autem quidem consequuntur hic alias at? Libero, ipsum aperiam?
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem culpa earum fuga veniam non eos in, sed nisi illo cumque aspernatur assumenda voluptates, sunt soluta, id magni placeat explicabo dignissimos!
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit aperiam, dolorem magni molestiae enim nisi laudantium deserunt asperiores soluta excepturi similique, nulla quaerat doloremque eius reiciendis voluptatibus animi, obcaecati corrupti.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sequi dignissimos quo repellat exercitationem accusamus dolorum ab, ullam voluptates dicta eum neque assumenda? Inventore soluta explicabo ea asperiores voluptates totam.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum aut magni laboriosam ipsam hic repellat aperiam delectus ipsum eveniet possimus sapiente quae, adipisci tempora voluptas provident distinctio! Iusto, consectetur consequatur!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ad corporis repellendus ducimus quas molestiae tenetur quos necessitatibus commodi reiciendis libero possimus, reprehenderit adipisci dignissimos illo non aliquid blanditiis suscipit!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis numquam ad rerum corrupti nisi tempore ratione quisquam velit laudantium sit voluptates expedita, labore quasi, dolorum accusamus necessitatibus sequi. Magni, architecto.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum sit voluptatem facilis excepturi officiis dolores. Perspiciatis amet ipsum dolores ipsam, repellat atque. Soluta qui nostrum quae culpa quod voluptate ad!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim perferendis, nihil adipisci ratione exercitationem earum aliquid minima optio ipsa commodi culpa ea delectus pariatur sequi id corrupti, ad dolores dicta?
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus reiciendis veniam quas, labore harum voluptas perferendis autem consequuntur nesciunt consectetur voluptatem explicabo ad laudantium, est inventore minima, illum obcaecati dignissimos!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium molestiae alias repellat deleniti. Possimus, et. Esse, eveniet nemo porro, quis ullam magnam atque maxime iusto ratione maiores corporis, facilis ut!
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, maiores, distinctio similique tempora quod repellat voluptas, eligendi dolore non eum pariatur exercitationem perspiciatis delectus eius laboriosam dolores! Placeat, necessitatibus obcaecati.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut molestiae harum animi perspiciatis tenetur quam, dolorem, quis aperiam repudiandae assumenda modi autem quidem consequuntur hic alias at? Libero, ipsum aperiam?
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem culpa earum fuga veniam non eos in, sed nisi illo cumque aspernatur assumenda voluptates, sunt soluta, id magni placeat explicabo dignissimos!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit aperiam, dolorem magni molestiae enim nisi laudantium deserunt asperiores soluta excepturi similique, nulla quaerat doloremque eius reiciendis voluptatibus animi, obcaecati corrupti.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sequi dignissimos quo repellat exercitationem accusamus dolorum ab, ullam voluptates dicta eum neque assumenda? Inventore soluta explicabo ea asperiores voluptates totam.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum aut magni laboriosam ipsam hic repellat aperiam delectus ipsum eveniet possimus sapiente quae, adipisci tempora voluptas provident distinctio! Iusto, consectetur consequatur!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ad corporis repellendus ducimus quas molestiae tenetur quos necessitatibus commodi reiciendis libero possimus, reprehenderit adipisci dignissimos illo non aliquid blanditiis suscipit!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis numquam ad rerum corrupti nisi tempore ratione quisquam velit laudantium sit voluptates expedita, labore quasi, dolorum accusamus necessitatibus sequi. Magni, architecto.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum sit voluptatem facilis excepturi officiis dolores. Perspiciatis amet ipsum dolores ipsam, repellat atque. Soluta qui nostrum quae culpa quod voluptate ad!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim perferendis, nihil adipisci ratione exercitationem earum aliquid minima optio ipsa commodi culpa ea delectus pariatur sequi id corrupti, ad dolores dicta?
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus reiciendis veniam quas, labore harum voluptas perferendis autem consequuntur nesciunt consectetur voluptatem explicabo ad laudantium, est inventore minima, illum obcaecati dignissimos!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium molestiae alias repellat deleniti. Possimus, et. Esse, eveniet nemo porro, quis ullam magnam atque maxime iusto ratione maiores corporis, facilis ut!
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, maiores, distinctio similique tempora quod repellat voluptas, eligendi dolore non eum pariatur exercitationem perspiciatis delectus eius laboriosam dolores! Placeat, necessitatibus obcaecati.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut molestiae harum animi perspiciatis tenetur quam, dolorem, quis aperiam repudiandae assumenda modi autem quidem consequuntur hic alias at? Libero, ipsum aperiam?
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem culpa earum fuga veniam non eos in, sed nisi illo cumque aspernatur assumenda voluptates, sunt soluta, id magni placeat explicabo dignissimos!
+                    </p>
+                    <!--<?php
 
                    
                     if (isset($_POST['excluir'])) {
@@ -61,10 +111,10 @@ if($_POST['cpf_aluno']==NULL){
                         $treino->delete($id);
                     }
                     foreach ($treino->findTreinoAluno() as $key => $value) {
-                    ?>
+                    ?>-->
                         <tr>
-                            <td class="table_body"> <?php echo $value->nome_treino; ?> </td>
-                            <td class="table_body"> <?php echo $value->dt_treino; ?> </td>
+                            <td class="table_body"> <!--<?php echo $value->nome_treino; ?>--> </td>
+                            <td class="table_body"> <!--<?php echo $value->dt_treino; ?>--> </td>
                             <td>
                                 <form action="../visualizarFicha/index.php" method="post">
                                     <button type="submit" name="visualizar">
@@ -86,7 +136,7 @@ if($_POST['cpf_aluno']==NULL){
 
                             </td>
                         </tr>
-                    <?php } ?>
+                    <!--<?php } ?>-->
 
                 </tbody>
             </table>
